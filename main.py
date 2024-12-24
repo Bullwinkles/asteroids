4d857e094a49688b717142b81e0f364efbaf98cd
Bullwinkles <nolesfan7@gmail.com>
2024-12-24 15:11:23 -0500

    Added bullets and cleaned up code.

--- main.py
+++ main.py
@@ -4,18 +4,24 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
 	dt = 0
+	
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
 	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
+	
 	Player.containers = (updatable, drawable)
 	Asteroid.containers = (asteroids, updatable, drawable)
 	AsteroidField.containers = (updatable)
+	Shot.containers = (shots, drawable, updatable)
+	
 	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2,)
 	asteroidfield = AsteroidField()
 	while True:
@@ -24,11 +30,6 @@ def main():
 			if event.type == pygame.QUIT:
 				return
 		
-		screen.fill((0,0,0))
-		
-		for obj in drawable:
-			obj.draw(screen)
-		
 		for obj in updatable:
 			obj.update(dt)
 
@@ -36,6 +37,11 @@ def main():
 			if obj.collision(player):
 				print("Game Over!")
 				sys.exit()
+		
+		screen.fill((0,0,0))
+		
+		for obj in drawable:
+			obj.draw(screen)
 		
 		pygame.display.flip()
 		
